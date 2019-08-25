43823c9e52aa9a0b55b7e0f15ae479e3bf286f0c
shadiqurshaon <shadiqurshaon@gmail.com>
2019-08-25 19:01:02 +0600

    mordgase calculator

--- resources/views/propertyResult.blade.php
+++ resources/views/propertyResult.blade.php
@@ -79,7 +79,7 @@
                 </table>
 
 
-                <div style="border-top:10px solid #7b88ff; padding-top: 20px;">
+                <div style="border-top:10px solid #7b88ff; padding-top: 20px; margin-bottom: 30px">
                     <div class="row">
                         <div class="col-md-6">
                             We are here to help.
@@ -97,7 +97,65 @@
 
 {{--                <div>maps content</div>--}}
             </div>
-            <div class="col-md-12">
+            <div class="col-md-4" >
+                <div style="height: 400px; margin-bottom: 50PX">
+                    <h3>Mortgage Calculator</h3>
+
+                    <p>Amount:</p> <p id="amount" style="color: green"></p>
+                    <form onsubmit="event.preventDefault()">
+                        <div class="form-group">
+                            <label>Home Price</label>
+                            <input type="number" name="home_price" class="form-control" id="home_price" placeholder="$-00">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Down Payment</label>
+                            <input type="number" class="form-control" id="down_payment" placeholder="$-00">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Loan Term</label>
+                            <select class="form-control" id="longterm">
+                                <option value="1">1 Year</option>
+                                <option value="2">2 Year</option>
+                                <option value="3">3 Year</option>
+                                <option value="4">4 year</option>
+                                <option value="5">5 year</option>
+                                <option value="6">6 year</option>
+                                <option value="7">7 year</option>
+                                <option value="8">8 year</option>
+                                <option value="9">9 year</option>
+                                <option value="10">10 year</option>
+                                <option value="11">11 year</option>
+                                <option value="12">12 year</option>
+                                <option value="13">13 year</option>
+                                <option value="14">14 year</option>
+                                <option value="15">15 year</option>
+                                <option value="16">16 year</option>
+                                <option value="17">17 year</option>
+                                <option value="18">18 year</option>
+                                <option value="19">19 year</option>
+                                <option value="20">20 year</option>
+                                <option value="21">21 year</option>
+                                <option value="22">22 year</option>
+                                <option value="23">23 year</option>
+                                <option value="24">24 year</option>
+                                <option value="25">25 year</option>
+                                <option value="26">26 year</option>
+                                <option value="27">27 year</option>
+                                <option value="28">28 year</option>
+                                <option value="29">29 year</option>
+                                <option value="30">30 year</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Interest</label>
+                            <input type="number" class="form-control" id="interest" placeholder="$-00">
+                        </div>
+
+                        <button onclick="formsubmit()"  class="btn btn-default">Submit</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-8">
                 <div id="map"></div>
 
             </div>
@@ -390,6 +448,31 @@
 </section>
 
 <script>
+
+    function formsubmit() {
+
+        $("form").submit(function(e){
+            e.preventDefault();
+
+            return false;
+
+        });
+        var home_prize = $("#home_price").val();
+        var down_payment = $("#down_payment").val();
+        var lone_term = $("#longterm").val();
+        var interest = $("#interest").val();
+
+        var p = home_prize - down_payment;
+        var i = interest/100/12;
+        var n = lone_term*12;
+
+        console.log(p+i+n);
+        var monthly_amount =  p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
+
+        alert(monthly_amount);
+
+        $('#amount').text(monthly_amount);
+    }
     // Initialize and add the map
     function initMap() {
         // The location of Uluru
