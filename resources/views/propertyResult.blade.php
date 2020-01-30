62d77115395e6214fc95090b606cfeabd1f8fe31
Shadiqurshaon <shadiqurshaon@gmail.com>
2020-01-30 10:02:35 +0600

    comment out the recomendent property are commented in property result page

--- resources/views/propertyResult.blade.php
+++ resources/views/propertyResult.blade.php
@@ -145,7 +145,7 @@
 
                 $link = "https://www.google.com/maps/embed/v1/streetview?location=".$lat_long."&key=AIzaSyBnSQ_kM3vMc0p2pjZkblR3osUx7sJ23kA";
                  ?>
-                <iframe width="600" height="450" frameborder="0" style="border:0" src="{{$link}}" allowfullscreen></iframe>
+                <iframe width="555" height="450" frameborder="0" style="border:0" src="{{$link}}" allowfullscreen></iframe>
 
                 <div style="margin-top: 5px;">
                 <div id="map"></div>
@@ -231,211 +231,18 @@
                         </div>
                         <div class="row portfolio-items">
 
-                            @foreach($recomendentAddresses as $address)
+                           
 
-                            <div class="item col-lg-3 col-md-6 col-xs-12 landscapes">
-                                <div class="project-single">
-                                    <div class="project-inner project-head">
-                                        <div class="project-bottom">
-                                            <h4><a href="{{route('propertyResult',['propertyId' => $address['addressInfo']['id']])}}">View Property</a><span class="category">Real Estate</span></h4>
-                                        </div>
-                                        <div class="homes">
-                                            <!-- homes img -->
-                                            <a href="properties-details.html" class="homes-img">
-                                                <div class="homes-tag button alt featured">Featured</div>
-                                                <div class="homes-tag button alt sale">For Sale</div>
-                                                <div class="homes-price">Family Home</div>
-                                                <img src="{{asset('images/feature-properties/fp-1.jpg')}}" alt="home-1" class="img-responsive">
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <!-- homes content -->
-                                    <div class="homes-content">
-                                        <!-- homes address -->
-                                        <h3>Real House Luxury Villa</h3>
-                                        <p class="homes-address mb-3">
-                                            <a href="properties-details.html">
-                                                <i class="fa fa-map-marker"></i><span>Est St, 77 - Central Park South, NYC</span>
-                                            </a>
-                                        </p>
-
-                                        <div class="footer">
-                                            <!-- Price -->
-                                            <div class="price-properties">
-                                                <h3 class="title mt-3">
-                                                <a href="properties-details.html">$ 230,000</a>
-                                                </h3>
-                                                <div class="compare">
-                                                    <a href="#" title="Compare">
-                                                        <i class="fas fa-exchange-alt"></i>
-                                                    </a>
-                                                    <a href="#" title="Share">
-                                                        <i class="fas fa-share-alt"></i>
-                                                    </a>
-                                                    <a href="#" title="Favorites">
-                                                        <i class="fa fa-heart-o"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            @endforeach
-
-
-
-
-
-                            <div class="item col-lg-3 col-md-6 col-xs-12 people">
-                                <div class="project-single">
-                                    <div class="project-inner project-head">
-                                        <div class="project-bottom">
-                                            <h4><a href="properties-details.html">View Property</a><span class="category">Real Estate</span></h4>
-                                        </div>
-                                        <div class="homes">
-                                            <!-- homes img -->
-                                            <a href="properties-details.html" class="homes-img">
-                                                <div class="homes-tag button alt featured">Featured</div>
-                                                <div class="homes-tag button sale rent">For Rent</div>
-                                                <div class="homes-price">Family Home</div>
-                                                <img src="{{asset('images/feature-properties/fp-2.jpg')}}" alt="home-1" class="img-responsive">
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <!-- homes content -->
-                                    <div class="homes-content">
-                                        <!-- homes address -->
-                                        <h3>Real House Luxury Villa</h3>
-                                        <p class="homes-address mb-3">
-                                            <a href="properties-details.html">
-                                                <i class="fa fa-map-marker"></i><span>Est St, 77 - Central Park South, NYC</span>
-                                            </a>
-                                        </p>
-                                        <div class="footer">
-                                            <!-- Price -->
-                                            <div class="price-properties">
-                                                <h3 class="title mt-3">
-                                                <a href="properties-details.html">$ 230,000</a>
-                                                </h3>
-                                                <div class="compare">
-                                                    <a href="#" title="Compare">
-                                                        <i class="fas fa-exchange-alt"></i>
-                                                    </a>
-                                                    <a href="#" title="Share">
-                                                        <i class="fas fa-share-alt"></i>
-                                                    </a>
-                                                    <a href="#" title="Favorites">
-                                                        <i class="fa fa-heart-o"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                       
 
 
 
 
 
 
-                            <div class="item col-lg-3 col-md-6 col-xs-12 people landscapes no-pb pbp-3">
-                                <div class="project-single no-mb mbp-3">
-                                    <div class="project-inner project-head">
-                                        <div class="project-bottom">
-                                            <h4><a href="properties-details.html">View Property</a><span class="category">Real Estate</span></h4>
-                                        </div>
-                                        <div class="homes">
-                                            <!-- homes img -->
-                                            <a href="properties-details.html" class="homes-img">
-                                                <div class="homes-tag button alt featured">Featured</div>
-                                                <div class="homes-tag button alt sale">For Sale</div>
-                                                <div class="homes-price">Family Home</div>
-                                                <img src="{{asset('images/feature-properties/fp-3.jpg')}}" alt="home-1" class="img-responsive">
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <!-- homes content -->
-                                    <div class="homes-content">
-                                        <!-- homes address -->
-                                        <h3>Real House Luxury Villa</h3>
-                                        <p class="homes-address mb-3">
-                                            <a href="properties-details.html">
-                                                <i class="fa fa-map-marker"></i><span>Est St, 77 - Central Park South, NYC</span>
-                                            </a>
-                                        </p>
-
-                                        <div class="footer">
-                                            <!-- Price -->
-                                            <div class="price-properties">
-                                                <h3 class="title mt-3">
-                                                <a href="properties-details.html">$ 230,000</a>
-                                                </h3>
-                                                <div class="compare">
-                                                    <a href="#" title="Compare">
-                                                        <i class="fas fa-exchange-alt"></i>
-                                                    </a>
-                                                    <a href="#" title="Share">
-                                                        <i class="fas fa-share-alt"></i>
-                                                    </a>
-                                                    <a href="#" title="Favorites">
-                                                        <i class="fa fa-heart-o"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item col-lg-3 col-md-6 col-xs-12 people landscapes no-pb">
-                                <div class="project-single no-mb">
-                                    <div class="project-inner project-head">
-                                        <div class="project-bottom">
-                                            <h4><a href="properties-details.html">View Property</a><span class="category">Real Estate</span></h4>
-                                        </div>
-                                        <div class="homes">
-                                            <!-- homes img -->
-                                            <a href="properties-details.html" class="homes-img">
-                                                <div class="homes-tag button alt featured">Featured</div>
-                                                <div class="homes-tag button sale rent">For Rent</div>
-                                                <div class="homes-price">Family Home</div>
-                                                <img src="{{asset('images/feature-properties/fp-4.jpg')}}" alt="home-1" class="img-responsive">
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <!-- homes content -->
-                                    <div class="homes-content">
-                                        <!-- homes address -->
-                                        <h3>Real House Luxury Villa</h3>
-                                        <p class="homes-address mb-3">
-                                            <a href="properties-details.html">
-                                                <i class="fa fa-map-marker"></i><span>Est St, 77 - Central Park South, NYC</span>
-                                            </a>
-                                        </p>
-
-                                        <div class="footer">
-                                            <!-- Price -->
-                                            <div class="price-properties">
-                                                <h3 class="title mt-3">
-                                                <a href="properties-details.html">$ 230,000</a>
-                                                </h3>
-                                                <div class="compare">
-                                                    <a href="#" title="Compare">
-                                                        <i class="fas fa-exchange-alt"></i>
-                                                    </a>
-                                                    <a href="#" title="Share">
-                                                        <i class="fas fa-share-alt"></i>
-                                                    </a>
-                                                    <a href="#" title="Favorites">
-                                                        <i class="fa fa-heart-o"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                          
+                        
+                              
                             </div>
                         </div>
                     </div>
